1432da381d1d6fd28c75b7fa90fd0c3a395ff808
simbindiganavale <43503074+simbindiganavale@users.noreply.github.com>
2019-10-04 00:18:59 +0200

    Moved the project to repository

---
+++ holidayManager/src/main/java/com/application/employeePortal/holidayManager/domain/entities/Employee.java
@@ -0,0 +1,30 @@
+package com.application.employeePortal.holidayManager.domain.entities;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="EMPLOYEE")
+public class Employee {
+    @Id
+    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long employeeId;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee")
+    private List<Leave>leaves;
+}
---
+++ holidayManager/src/main/java/com/application/employeePortal/holidayManager/domain/entities/LeaveJournal.java
@@ -0,0 +1,31 @@
+package com.application.employeePortal.holidayManager.domain.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="LEAVE_JOURNAL")
+public class LeaveJournal {
+    @Id
+    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    @Column(name = "AVAILABLE_LEAVES")
+    private int numberOfLeaves;
+    @Column(name = "YEAR")
+    private int year;
+    @ManyToOne(cascade = {CascadeType.PERSIST}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "EMPLOYEE_ID")
+    private Employee employee;
+}
